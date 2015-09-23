b1126bd20dc6fee4931006754628b294529bddfc
Andrew Nystrom <awnystrom@gmail.com>
2015-09-23 16:08:41 -0500

    packaged

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "simulated_annealing",
+    packages = find_packages(),
+    version = "0.1.0",
+    author='Skyler Grammer',
+    description = "A Simulated Annealing implimentation with a scikit-learn style API backed by joblib for speed.",
+    url = "https://github.com/skylergrammer/SimulatedAnnealing",
+    keywords = ["simulated", "annealing", "parameter", "hyperparameter", "optimization", "optimisation", "model", "selection"],
+    license = "Apache 2.0",
+    install_requires = ['scikit-learn>=0.16.0', 'numpy>=1.9.0'],
+    classifiers = ["Programming Language :: Python", 
+    			   "Programming Language :: Python :: 2.7",
+    			   "Programming Language :: Python :: 2",
+    			   "License :: OSI Approved :: Apache Software License",
+    			   "Operating System :: OS Independent",
+    			   "Development Status :: 4 - Beta",
+    			   "Intended Audience :: Developers"
+    			   ]
+	)
